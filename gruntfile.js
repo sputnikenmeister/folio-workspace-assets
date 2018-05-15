322498bd686d65f5d3a34d94b66ed3bd76eb9dfe
Pablo Canillas <pablo@canillas.name>
2018-05-15 14:02:27 +0100

    grunt favicon building revamped

--- gruntfile.js
+++ gruntfile.js
@@ -21,8 +21,9 @@ module.exports = function(grunt) {
 		ext: {
 			fonts: "*.{eot,woff,woff2,svg,ttf,otf}",
 		},
-		dev: {
-			appRoot: "http://localhost/projects/folio-sym",
+		web: {
+			root: "http://localhost/projects/folio-sym",
+			workspace: "<%= paths.web.root %>/workspace"
 		},
 		filebase: {
 			debugClientJs: "folio-debug-client",
@@ -30,33 +31,36 @@ module.exports = function(grunt) {
 			debugStyles: "folio-debug",
 			distJs: "folio",
 			distStyles: "folio",
+			ieStyles: "folio-ie",
 		}
 	});
-
-	grunt.loadTasks("./build/grunt/tasks");
 
 	/* --------------------------------
 	/* Main Targets
 	/* -------------------------------- */
 
+	// resources
 	grunt.registerTask("deps-styles", ["copy:resources", "copy:sources", "compass:fonts"]);
-	grunt.registerTask("deps-js", ["modernizr-build:production"]);
+	grunt.registerTask("deps", ["deps-styles", "build-favicon", "modernizr-build:production"]);
 
-	grunt.registerTask("debug-styles", ["compass:debug", "autoprefixer:debug"]);
+	// debug build tasks
+	grunt.registerTask("debug-styles", ["compass:debug", "compass:fonts", "compass:ie", "autoprefixer:debug"]);
 	grunt.registerTask("debug-vendor", ["browserify:vendor", "exorcise:vendor"]);
 	grunt.registerTask("debug-client", ["browserify:client", "exorcise:client"]);
-	grunt.registerTask("debug", ["debug-vendor", "debug-client", "debug-styles"]);
 
-	grunt.registerTask("dist-styles", ["compass:dist", "compass:fonts", "autoprefixer:dist"]);
+	// dist build tasks
+	grunt.registerTask("dist-styles", ["compass:dist", "compass:fonts", "compass:ie", "autoprefixer:dist"]);
 	grunt.registerTask("dist-js", ["browserify:dist", "uglify:dist"]);
+
+	grunt.registerTask("debug-watch", ["browserify:watch-client", "browserify:watch-vendor", "watch"]);
+	grunt.registerTask("debug", ["debug-vendor", "debug-client", "debug-styles"]);
 	grunt.registerTask("dist", ["dist-js", "dist-styles"]);
 
 	grunt.registerTask("clean-all", ["clean:js", "clean:css", "compass:clean"]);
 	grunt.registerTask("build", ["debug", "dist"]);
-	grunt.registerTask("rebuild", ["clean-all", "deps-js", "build"]);
+	grunt.registerTask("rebuild", ["clean-all", "deps", "build"]);
 	// Default task
-	grunt.registerTask("default", ["browserify:watch-client", "browserify:watch-vendor", "watch"]);
-	// grunt.registerTask("default", ["build-watch"]);
+	grunt.registerTask("default", ["rebuild", "debug-watch"]);
 
 	/* --------------------------------
 	/* watch
@@ -71,14 +75,15 @@ module.exports = function(grunt) {
 		"reload-config": {
 			// options: { spawn: true },
 			files: ["gruntfile.js", "package.json"],
-			tasks: ["clean-all",
-				"modernizr-build:production",
-				"compass:fonts", "compass:debug", "autoprefixer:debug",
-				"browserify:vendor", "browserify:client"
-			],
+			tasks: ["rebuild"],
+			// tasks: ["clean-all",
+			// 	"modernizr-build:production",
+			// 	"compass:fonts", "compass:debug", "autoprefixer:debug",
+			// 	"browserify:vendor", "browserify:client"
+			// ],
 		},
 		"build-styles": {
-			tasks: ["compass:debug", "autoprefixer:debug"],
+			tasks: ["compass:debug", "compass:ie", "autoprefixer:debug"],
 			files: ["src/sass/**/*.scss", "src/sass/**/*.json"],
 		},
 		"build-deps": {
@@ -119,10 +124,10 @@ module.exports = function(grunt) {
 				expand: true,
 				dest: "./fonts/",
 				src: [
-					"./node_modules/@folio/webfonts/build/fonts/franklin-gothic-itc-cp/<%= paths.ext.fonts %>",
 					"./node_modules/@folio/webfonts/build/fonts/folio/<%= paths.ext.fonts %>",
 					"./src/resources/fonts/franklin-gothic-fs/<%= paths.ext.fonts %>",
-					"./src/resources/fonts/fontello*/**/<%= paths.ext.fonts %>",
+					// "./node_modules/@folio/webfonts/build/fonts/franklin-gothic-itc-cp/<%= paths.ext.fonts %>",
+					// "./src/resources/fonts/fontello*/**/<%= paths.ext.fonts %>",
 				]
 			}]
 		},
@@ -138,7 +143,7 @@ module.exports = function(grunt) {
 				cwd: "./node_modules/@folio/webfonts/build/sass/",
 				src: [
 					"_folio.scss",
-					"_franklin-gothic-itc-cp.scss",
+					// "_franklin-gothic-itc-cp.scss",
 				]
 			}]
 		}
@@ -147,6 +152,29 @@ module.exports = function(grunt) {
 	/* ---------------------------------
 	/* Style Sheets
 	/* --------------------------------- */
+
+	/* sass
+	/* `npm install --save-dev grunt-sass`
+	/* `npm install --save-dev node-sass-json-importer node-sass-import-once`
+	/* - - - - - - - - - - - - - - - - - */
+	// var sassJsonImporter = require('node-sass-json-importer');
+	// var sassImportOnce = require('node-sass-import-once');
+	//
+	// grunt.loadNpmTasks("grunt-sass");
+	// grunt.config("sass", {
+	// 	options: {
+	// 		includePaths: ["src/sass/", "build/compass-stylesheets/"],
+	// 		importer: [sassJsonImporter] //, sassImportOnce],
+	// 	},
+	// 	debug: {
+	// 		options: {
+	// 			sourceMap: true,
+	// 		},
+	// 		files: {
+	// 			"css/folio-debug.css": "src/sass/<%= paths.filebase.debugStyles %>.scss"
+	// 		}
+	// 	}
+	// });
 
 	/* NOTE: compass dependencies
 	 *`gem install compass sass-json-vars compass-import-once`
@@ -159,7 +187,6 @@ module.exports = function(grunt) {
 		require: [
 			"sass-json-vars",
 			"compass-import-once",
-			// "compass/import-once",
 			"./build/grunt/compass-encode.rb", // alternative to compass inline-image()
 		],
 		sassDir: "src/sass",
@@ -186,6 +213,11 @@ module.exports = function(grunt) {
 		// outputStyle: "compressed",
 	});
 
+	grunt.config("compass.ie.options", {
+		specify: ["src/sass/<%= paths.filebase.ieStyles %>.scss"],
+		sourcemap: true,
+	});
+
 	/* autoprefixer
 	/* - - - - - - - - - - - - - - - - - */
 	grunt.loadNpmTasks("grunt-autoprefixer");
@@ -200,6 +232,7 @@ module.exports = function(grunt) {
 	/* Build JS dependencies
 	/* -------------------------------- */
 
+	grunt.loadTasks("./build/grunt/tasks");
 	grunt.config("modernizr-build.production", {
 		files: {
 			"./build/generated/js/modernizr-dist.js": [
@@ -259,16 +292,14 @@ module.exports = function(grunt) {
 	/* browserify:client */
 	grunt.config("browserify.client", {
 		dest: "./js/<%= paths.filebase.debugClientJs %>.js",
-		// dest: "./build/tasks/browserify.client/<%= paths.filebase.debugClientJs %>.js",
 		src: ["./src/js/app/App.js"],
 		options: {
 			browserifyOptions: {
 				debug: true,
 				fullPaths: false,
 				insertGlobalVars: {
-					DEBUG: function(file, dir) {
-						return true;
-					}
+					DEBUG: function(file, dir) { return true; },
+					GA: function(file, dir) { return true; },
 				}
 			},
 			transform: [
@@ -361,11 +392,10 @@ module.exports = function(grunt) {
 				browserifyOptions: {
 					debug: false,
 					fullPaths: false,
-					// insertGlobalVars: {
-					// 	DEBUG: function(file, dir) {
-					// 		return false;
-					// 	}
-					// }
+					insertGlobalVars: {
+						DEBUG: function(file, dir) { return false; },
+						GA: function(file, dir) { return true; }
+					}
 				},
 			},
 			grunt.config("browserify.vendor.options"),
@@ -395,7 +425,8 @@ module.exports = function(grunt) {
 				dead_code: true,
 				drop_console: true,
 				global_defs: {
-					"DEBUG": false
+					"DEBUG": false,
+					"GA": true,
 				},
 			}
 		},
@@ -426,65 +457,138 @@ module.exports = function(grunt) {
 	/* generate-favicons
 	 * NOTE: requires `brew install imagemagick`
 	 * - - - - - - - - - - - - - - - - - */
+	var faviconColor = "#D0021B";
+	grunt.config("paths.favicons", {
+		file: "profile-abstract2-black.png",
+		src: "src/resources/favicons",
+		dest: "images/favicons",
+		generated: "<%= paths.src.generated %>/favicons",
 
-	// grunt.loadNpmTasks("grunt-contrib-clean");
+	});
 	// NOTE: already loaded above
+	// grunt.loadNpmTasks("grunt-contrib-clean");
 	grunt.config("clean.favicons", {
 		src: [
-			"<%= paths.src.generated %>/resources/favicons",
-			"./images/favicons"
-		]
+				"<%= paths.favicons.generated %>",
+				"<%= paths.favicons.dest %>",
+			]
 	});
+
+	grunt.config("copy.svg-favicon", {
+		files: [{
+			src: "<%= paths.favicons.src %>/<%= paths.favicons.file %>",
+			dest: "<%= paths.favicons.generated %>/favicon.png",
+		}]
+	});
+
+	var obj = {
+		templateData: [],
+		globals: [{
+			maskRadius: "50%",
+			viewBox: "0 0 512 512",
+			transform: "translate(256, 256) scale(1.05) translate(-256, -256)"
+		}],
+		files: [{
+			src: "<%= paths.favicons.src %>/favicon_template.hbs",
+			dest: []
+		}],
+	};
+
+	var sizes;
+	// sizes = [16, 32, 48, 64, 128, 256, 512];
+	sizes = [57, 72, 114, 120, 144, 152];
+	obj = sizes.reduce(function(acc, val, idx, arr) {
+		// grunt.log.writeln("args: " + JSON.stringify(arguments));
+		acc.files[0].dest[idx] = "<%= paths.favicons.generated %>"
+			+ "/apple-touch-icon-" + val + "x" + val + ".svg";
+		acc.templateData[idx] = {
+			location: "./favicon.png",
+			size: val,
+		};
+		return acc;
+	}, obj);
+	obj.files[0].dest.push("<%= paths.favicons.generated %>/favicon_roundel.svg");
+	obj.templateData.push({ location: "./favicon.png", size: 600 });
+
+	grunt.loadNpmTasks('grunt-compile-handlebars');
+	grunt.config("compile-handlebars.svg-wrap", obj);
+
+
 	grunt.loadNpmTasks("grunt-svg2png");
 	grunt.config("svg2png.favicons", {
-		files: [{
-			cwd: "src/resources/favicons/",
-			src: "IMG_0139_*.svg",
-			dest: "<%= paths.src.generated %>/resources/favicons/"
-		}]
+		files: [
+		// 	{
+		// 		cwd: "<%= paths.favicons.src %>/",
+		// 		src: ["*.svg"],
+		// 		dest: "<%= paths.favicons.generated %>/"
+		// },
+			{
+				cwd: "<%= paths.favicons.generated %>/",
+				src: ["apple-touch-icon-*.svg"],
+				dest: "<%= paths.favicons.dest %>/"
+		}, {
+				cwd: "<%= paths.favicons.generated %>/",
+				src: ["favicon_roundel.svg"],
+				dest: "<%= paths.favicons.generated %>/"
+				/* this plugin seems to be using src as cwd, and swaps the file ext to png :( */
+				// dest: "./<%= paths.favicons.generated %>/"
+		}
+	]
 	});
 	grunt.loadNpmTasks("grunt-favicons");
 	grunt.config("favicons", {
 		options: {
-			trueColor: true,
-			tileBlackWhite: false,
+			// debug: false,
+			apple: false,
+			regular: false,
+			windowsTile: false,
+			timestamp: true
 		},
+		square: {
+			options: {
+				trueColor: false,
+				tileColor: faviconColor,
+				windowsTile: true,
+				tileBlackWhite: false,
+				apple: false,
+				regular: false,
+				html: "<%= paths.favicons.generated %>/favicon_square.html",
+				HTMLPrefix: "/workspace/assets/images/favicons/",
+			},
+			src: "<%= paths.favicons.generated %>/favicon.png",
+			dest: "<%= paths.favicons.dest %>/",
+		},
+		roundel: {
+			options: {
+				apple: true,
+				regular: true,
+				trueColor: true,
+				precomposed: true,
+				appleTouchBackgroundColor: faviconColor,
+				appleTouchPadding: 0,
+				html: "<%= paths.favicons.generated %>/favicon_roundel.html",
+				HTMLPrefix: "/workspace/assets/images/favicons/",
+			},
+			src: "<%= paths.favicons.generated %>/favicon_roundel.png",
+			dest: "<%= paths.favicons.dest %>/",
+		},
+		/*
 		steampunk: {
 			options: {
-				appleTouchPadding: 10,
 				appleTouchBackgroundColor: "#FEFCE7",
 				tileColor: "#FEFCE7",
-				html: "<%= paths.src.generated %>/resources/favicons/steampunk.html",
-				HTMLPrefix: "/workspace/assets/images/favicons/steampunk/",
 			},
 			src: "src/resources/favicons/steampunk.png",
-			dest: "images/favicons/steampunk"
-		},
-		img_0139_square: {
-			options: {
-				html: "<%= paths.src.generated %>/resources/favicons/img_0139_square.html",
-				HTMLPrefix: "/workspace/assets/images/favicons/square/",
-				appleTouchPadding: 0,
-			},
-			src: "<%= paths.src.generated %>/resources/favicons/img_0139_square.png",
-			dest: "images/favicons/square",
-		},
-		img_0139_circle: {
-			options: {
-				html: "<%= paths.src.generated %>/resources/favicons/img_0139_circle.html",
-				HTMLPrefix: "/workspace/assets/images/favicons/circle/",
-				appleTouchPadding: 0,
-			},
-			src: "<%= paths.src.generated %>/resources/favicons/IMG_0139_circle.png",
-			dest: "images/favicons/circle",
-		},
+		}*/
 	});
-	grunt.registerTask("generate-favicons", [
+
+	grunt.registerTask("build-favicon", [
 		"clean:favicons",
+		"copy:svg-favicon",
+		"compile-handlebars:svg-wrap",
 		"svg2png:favicons",
-		"favicons:img_0139_square",
-		"favicons:img_0139_circle",
-		// "favicons:steampunk",
+		"favicons:square",
+		"favicons:roundel",
 	]);
 
 
@@ -497,7 +601,7 @@ module.exports = function(grunt) {
 			ignoreErrors: true
 		},
 		bootstrap: {
-			options: { url: "<%= paths.dev.appRoot %>/json" },
+			options: { url: "<%= paths.web.root %>/json" },
 			dest: "<%= paths.src.generated %>/js/bootstrap.js"
 		}
 	});
